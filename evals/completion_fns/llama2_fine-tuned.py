033e21b6d323069b5ddaacb37dc1c2d892c6470e
Andrew Beiler <abeiler@hopperv4.mcs.slu.edu>
2023-09-28 11:43:34 -0500

    Updates to improve inference speed for llama2 models and fixes to the custom big-bench-arithmetic eval

--- evals/completion_fns/llama2_fine-tuned.py
+++ evals/completion_fns/llama2_fine-tuned.py
@@ -1,6 +1,7 @@
 from evals.api import CompletionFn, CompletionResult
 from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
 import torch
+import time
 
 class LLaMA2FineTunedCompletionResult(CompletionResult):
     def __init__(self, response) -> None:
@@ -12,10 +13,10 @@ class LLaMA2FineTunedCompletionResult(CompletionResult):
 
 class LLaMA2FineTunedCompletionFn(CompletionFn):
     def __init__(self, model_id: str, base_model:str, **kwargs) -> None:
-        # llm = OpenAI(temperature=0)
-        # self.llm_math = LLMMathChain(llm=llm)
-        self.model = AutoModelForCausalLM.from_pretrained(model_id, load_in_8bit=True, torch_dtype=torch.bfloat16, device_map="auto")
+        self.model = AutoModelForCausalLM.from_pretrained(model_id, torch_dtype=torch.float16, device_map={"":torch.cuda.current_device()})
         self.tokenizer = AutoTokenizer.from_pretrained(base_model)
+
+        self.model.eval()
 
     def __call__(self, prompt, **kwargs) -> LLaMA2FineTunedCompletionResult:
 
@@ -26,17 +27,27 @@ class LLaMA2FineTunedCompletionFn(CompletionFn):
 
         # '''
 
-        pipe = pipeline(
-            "text-generation",
-            model=self.model,
-            tokenizer=self.tokenizer,
-            max_new_tokens=512,
-            temperature=0.7,
-            top_p=0.95,
-            repetition_penalty=1.15
-        )
-        response = pipe(prompt)
-        return LLaMA2FineTunedCompletionResult(response[0]['generated_text'])
+        #pipe = pipeline(
+        #    "text-generation",
+        #    model=self.model,
+        #    tokenizer=self.tokenizer,
+        #    max_new_tokens=512,
+        #    temperature=0.7,
+        #    top_p=0.95,
+        #    repetition_penalty=1.15,
+            #device=torch.cuda.current_device(),
+        #)
+        #response = pipe(prompt)
+
+        with torch.no_grad():
+            input_ids = self.tokenizer(prompt, return_tensors='pt').to("cuda")
+            #start = time.perf_counter()
+            output = self.model.generate(**input_ids, max_new_tokens=512)
+            response = self.tokenizer.decode(output[0], skip_special_tokens=True)
+            #e2e_inference_time = (time.perf_counter()-start)*1000
+            #print(f"the inference time is {e2e_inference_time} ms")
+
+        return LLaMA2FineTunedCompletionResult(response)
         # prompt = CompletionPrompt(prompt).to_formatted_prompt()
         # response = self.llm_math.run(prompt)
         # # The LangChain response comes with `Answer: ` ahead of this, let's strip it out
